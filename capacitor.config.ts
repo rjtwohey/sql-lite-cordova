0470f2bc73485e52f79b6080e4b00097fd7b07f2
Ryan Twohey <ryantwohey@Ryans-MBP-2.attlocal.net>
2022-11-07 14:08:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sql-lite-cordova',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
